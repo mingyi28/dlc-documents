0b6cc1ba73b1b05a051cfcd63f98456effdd492f
mingyiwang <mingyiwang@tencent.com>
2025-04-14 23:01:25 +0200

    path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { resolve } from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '404.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
